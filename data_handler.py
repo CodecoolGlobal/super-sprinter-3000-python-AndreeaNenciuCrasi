826a3b26b13c65a9676249589e964814998ce772
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2019-12-05 16:08:52 +0200

    Added add, result, update.html, created list.html, read and write functions for data.csv

--- data_handler.py
+++ data_handler.py
@@ -6,5 +6,30 @@ DATA_HEADER = ['id', 'title', 'user_story', 'acceptance_criteria', 'business_val
 STATUSES = ['planning', 'todo', 'in progress', 'review', 'done']
 
 
-def get_all_user_story():
-    return []
+def count_rows_in_file(file_name):
+    file = open(file_name, "r")
+    count = len(file.readlines())
+    file.close()
+    return count
+
+def get_all_user_story(filename):
+    dict_list = []
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            dict_list.append(row)
+    return dict_list
+
+def write_user_story(filename, mylist):
+    count = count_rows_in_file(filename)
+    mylist.insert(0, count)
+    with open('data.csv', 'a', newline='') as csvfile:
+        fieldnames = DATA_HEADER
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writerow({DATA_HEADER[0]: mylist[0], DATA_HEADER[1]: mylist[1], DATA_HEADER[2]: mylist[2], DATA_HEADER[3]: mylist[3],
+                         DATA_HEADER[4]: mylist[4], DATA_HEADER[5]: mylist[5], DATA_HEADER[6]: mylist[6]})
+
+# x = ['22', '33', '44', '55', '66', '77']
+# write_user_story('data.csv', x)
+# print(get_all_user_story('data.csv'))
--- server.py
+++ server.py
@@ -4,13 +4,36 @@ import data_handler
 
 app = Flask(__name__)
 
-
 @app.route('/')
 @app.route('/list')
 def route_list():
-    user_stories = data_handler.get_all_user_story()
+    user_stories = data_handler.get_all_user_story('data.csv')
 
     return render_template('list.html', user_stories=user_stories)
+
+@app.route('/add')
+def route_add():
+   return render_template('add.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   mylist = []
+   if request.method == 'POST':
+        result = request.form
+        for value in result.values():
+            mylist.append(value)
+        mylist.append('-')
+        data_handler.write_user_story('data.csv', mylist)
+        return render_template("result.html",result = result)
+   else:
+        return render_template('result.html')
+
+@app.route('/update')
+def route_edit():
+    statuses = ['planning', 'todo', 'in progress', 'review', 'done']
+    current = 'planning'
+
+    return render_template('update.html', statuses=statuses, selected_status=current)
 
 if __name__ == '__main__':
     app.run(
